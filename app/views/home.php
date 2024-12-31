f742c24456c80ffe873f98b8bb46dec607871299
Gabriel Mioni <gabrielmionibastos@gmail.com>
2024-12-31 00:08:36 -0300

    feature: Criando página home

---
+++ app/views/home.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>InfoCovid | Página Inicial</title>
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+<header>
+    <div class="container mt-5">
+        <h1 class="text-center">Bem-vindo à InfoCovid!</h1>
+    </div>
+</header>
+
+<main>
+    <div class="container text-center mt-4">
+        <p>Escolha um país para saber mais:</p>
+
+        <div class="btn-group" role="group" aria-label="Navegação por países">
+            <div class="">
+                <a href="brazil">
+                    <img src="https://www.worldometers.info/img/flags/br-flag.gif" alt="Bandeira do Brasil" class="country-image" height="64" width="64">
+                </a>
+            </div>
+
+            <div class="mx-3">
+                <a href="canada">
+                    <img src="https://www.worldometers.info/img/flags/ca-flag.gif" alt="Bandeira dos Canadá" class="country-image" height="64" width="64">
+                </a>
+            </div>
+
+            <div>
+                <a href="australia">
+                    <img src="https://www.worldometers.info/img/flags/as-flag.gif" alt="Bandeira da Austrália" class="country-image" height="64" width="64">
+                </a>
+            </div>
+        </div>
+    </div>
+</main>
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
