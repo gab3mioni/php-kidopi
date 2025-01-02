6d77c6efd16b21f4a06a5987460c3c6ca70561f2
Gabriel Mioni <gabrielmionibastos@gmail.com>
2025-01-02 15:16:27 -0300

    feature: Enviando dados ao footer para home

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -2,12 +2,22 @@
 
 namespace App\Controllers;
 
+use App\Services\CovidService;
 use Core\Controller;
 
 class HomeController extends Controller
 {
+    private CovidService $covidService;
+
+    public function __construct()
+    {
+        $this->covidService = new CovidService();
+    }
+
     public function index(): void
     {
-        $this->view('home');
+        $lastApiCall = $this->covidService->getLastApiCall();
+        $data['lastApiCall'] = $lastApiCall;
+        $this->view('home', $data);
     }
 }
